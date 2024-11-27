d153a4fb9a04fd3c8765289daee8818f8f4c13bd
vinayan <vinayanbalasubramanian@gmail.com>
2024-11-27 11:31:46 +0530

    step1

---
+++ script.js
@@ -0,0 +1,23 @@
+const countdown = () => {
+    const eventDate = new Date("December 31, 2024 23:59:59").getTime();
+    const now = new Date().getTime();
+    const gap = eventDate - now;
+  
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = Math.floor(gap / day);
+    const hours = Math.floor((gap % day) / hour);
+    const minutes = Math.floor((gap % hour) / minute);
+    const seconds = Math.floor((gap % minute) / second);
+  
+    document.getElementById("days").innerText = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+  };
+  
+  setInterval(countdown, 1000);
+  
